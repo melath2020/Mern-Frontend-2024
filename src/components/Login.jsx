f4e95cb74101c1751814b3963024b47bca269bf4
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-03 11:27:29 +0530

    updte

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 
 const Login = () => {
   const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [visible, setVisible] = useState(false)
   return (
     <>
     <div className='min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
@@ -28,6 +31,36 @@ const Login = () => {
                 className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'/>
               </div>
           </div>
+
+          <div>
+              <label htmlFor="password" className='block text-sm font-medium text-gray-700'>
+                Password
+              </label>
+              <div className="mt-1 relative">
+                <input type={visible ? "text" : "password"}  
+                name='password' 
+                autoComplete='current-password' 
+                required value={password} 
+                onChange={(e)=>setPassword(e.target.value)}
+                className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'/>
+               {
+                visible ? (
+                  <AiOutlineEye
+                  className="absolute right-2 top-2 cursor-pointer"
+                  size={25}
+                  onClick={() => setVisible(false)}
+                />
+                ): (
+                  <AiOutlineEyeInvisible
+                  className="absolute right-2 top-2 cursor-pointer"
+                  size={25}
+                  onClick={() => setVisible(true)}/>
+                )
+                 
+                
+               }
+              </div>
+            </div>
 
         </form>
 
