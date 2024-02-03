a157daef8171d253ca39a2d726bf731c019555db
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-03 11:18:30 +0530

    updte

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 
 const Login = () => {
+  const [email, setEmail] = useState("")
   return (
     <>
     <div className='min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
@@ -10,6 +11,28 @@ const Login = () => {
         <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
           Login to your Account
         </h2>
+        </div>
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className='bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+        <form  className='space-y-6'>
+          <div>
+          <label htmlFor="email" className='block text-sm font-medium text-gray-700'>
+                Email Address
+              </label>
+              <div className="mt-1">
+              <input type="text" 
+                name='email' 
+                autoComplete='email' 
+                required value={email} 
+                onChange={(e)=>setEmail(e.target.value)}
+                className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm'/>
+              </div>
+          </div>
+
+        </form>
+
+        </div>
+
         </div>
 
     </div>
