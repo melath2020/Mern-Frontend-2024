cefeb6e19d6b449a66530975f096557918b174e8
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-13 15:35:02 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { LoginPage,SignupPage,ActivationPage,HomePage,ProductsPage,BestSellingPage,EventsPage, FAQPage,ProductDetailsPage,ProfilePage } from './Routes'
 import "./App.css";
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import Store from './redux/store';
+import { loadUser } from './redux/actions/user';
 
 const App = () => {
+  useEffect(() => {
+    Store.dispatch(loadUser())
+    
+   }, [])
   return (
     <div>
     <BrowserRouter>
