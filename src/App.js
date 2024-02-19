32b07ce0881f9fa94ef5273afa81abb360c4a848
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-19 14:43:07 +0530

    upte

--- src/App.js
+++ src/App.js
@@ -1,42 +1,55 @@
-import React, { useEffect } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { LoginPage,SignupPage,ActivationPage,HomePage,ProductsPage,BestSellingPage,EventsPage, FAQPage,ProductDetailsPage,ProfilePage } from './Routes'
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  LoginPage,
+  SignupPage,
+  ActivationPage,
+  HomePage,
+  ProductsPage,
+  BestSellingPage,
+  EventsPage,
+  FAQPage,
+  ProductDetailsPage,
+  ProfilePage,
+} from "./Routes";
 import "./App.css";
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import Store from './redux/store';
-import { loadUser } from './redux/actions/user';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Store from "./redux/store";
+import { loadUser } from "./redux/actions/user";
 
 const App = () => {
   useEffect(() => {
-    Store.dispatch(loadUser())
-    
-   }, [])
+    Store.dispatch(loadUser());
+  }, []);
   return (
     <div>
-    <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<HomePage/>}/>
-    <Route path='/login' element={<LoginPage/>}/>
-    <Route path='/sign-up' element={<SignupPage/>}/>
-    <Route path='/activation/:activation_token' element={<ActivationPage/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignupPage />} />
+          <Route
+            path="/activation/:activation_token"
+            element={<ActivationPage />}
+          />
+        </Routes>
+      </BrowserRouter>
 
-<ToastContainer
-position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="dark"
-/>
-</div>
-  )
-}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
