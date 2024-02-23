921b646005769321289b3a8e895f91e2226dce33
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-02-23 12:20:14 +0530

    updte

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,6 +6,7 @@ import BestDeals from '../components/Route/BestDeals/BestDeals'
 import FeaturedProduct from '../components/Route/FeaturedProduct/FeaturedProduct'
 import Events from '../components/Events/Events'
 import Sponsored from '../components/Route/Sponsored'
+import Footer from '../components/Layout/Footer'
 
 const HomePage = () => {
   return (
@@ -17,6 +18,7 @@ const HomePage = () => {
         <Events/>
         <FeaturedProduct/>
         <Sponsored/>
+        <Footer/>
        
       
     </div>
