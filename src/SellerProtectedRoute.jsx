97712cf5679884864395a78ee8a4f5b23760916c
Aswinsudhakar <aswinsudhakaran98@gmail.com>
2024-03-18 14:10:59 +0530

    update

--- src/SellerProtectedRoute.jsx
+++ src/SellerProtectedRoute.jsx
@@ -2,10 +2,10 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const SellerProtectedRoute = ({ children }) => {
-  const { loading, isSeller } = useSelector((state) => state.seller);
-  if (loading === false) {
+  const { isLoading, isSeller } = useSelector((state) => state?.seller);
+  if (isLoading === false) {
     if (!isSeller) {
-      return <Navigate to={`/`} replace />;
+      return <Navigate to={`/shop-login`} replace />;
     }
     return children;
   }
