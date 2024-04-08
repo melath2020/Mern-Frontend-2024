a71cddeae3a8dc925a29341252f25a6375ddc452
Aswin <aswinsudhakaran98@gmail.com>
2024-04-08 11:14:40 +0530

    update

--- src/pages/Shop/ShopDashboardPage.jsx
+++ src/pages/Shop/ShopDashboardPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import DashboardHeader from '../../components/Shop/Layout/DashboardHeader'
+import DashboardSidebar from '../../components/Shop/Layout/DashboardSidebar'
 
 const ShopDashboardPage = () => {
   return (
     <div>
       <DashboardHeader/>
+      <div className="flex items-start justify-between w-full">
+      <div className="w-[80px] 800px:w-[330px]">
+        <DashboardSidebar/>
+
+        </div>
+
+      </div>
     </div>
   )
 }
