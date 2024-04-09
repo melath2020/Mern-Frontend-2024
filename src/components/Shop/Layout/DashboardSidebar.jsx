3c3a95bdc2403a23afdbe1b574109ac96982348c
Aswin <aswinsudhakaran98@gmail.com>
2024-04-09 11:42:17 +0530

    update

--- src/components/Shop/Layout/DashboardSidebar.jsx
+++ src/components/Shop/Layout/DashboardSidebar.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { AiOutlineFolderAdd, AiOutlineGift } from "react-icons/ai";
+import { FiPackage, FiShoppingBag } from "react-icons/fi";
+import { MdOutlineLocalOffer } from "react-icons/md";
+import { RxDashboard } from "react-icons/rx";
+import { VscNewFile } from "react-icons/vsc";
+import { CiMoneyBill, CiSettings } from "react-icons/ci";
+import { Link } from "react-router-dom";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { HiOutlineReceiptRefund } from "react-icons/hi";
 
-const DashboardSidebar = () => {
+const DashboardSidebar = ({ active }) => {
   return (
-    <div>
-      
+    <div className="w-full h-[90vh] bg-white shadow-sm overflow-y-scroll sticky top-0 left-0 z-10">
+        {/* single item */}
+        <div className="w-full flex items-center p-4">
+        <Link to="/dashboard" className="w-full flex items-center">
+          <RxDashboard
+            size={30}
+            color={`${active === 1 ? "crimson" : "#555"}`}
+          />
+          <h5
+            className={`hidden 800px:block pl-2 text-[18px] font-[400] ${
+              active === 1 ? "text-[crimson]" : "text-[#555]"
+            }`}
+          >
+            Dashboard
+          </h5>
+        </Link>
+      </div>
     </div>
   )
 }
