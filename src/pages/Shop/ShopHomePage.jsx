bb1a233ba46653418d02cbc3d226e5530796217b
Aswin <aswinsudhakaran98@gmail.com>
2024-04-10 18:35:49 +0530

    updte

--- src/pages/Shop/ShopHomePage.jsx
+++ src/pages/Shop/ShopHomePage.jsx
@@ -9,6 +9,10 @@ const ShopHomePage = () => {
        <div className="w-[25%] bg-[#fff] rounded-[4px] shadow-sm overflow-y-scroll h-[90vh] sticky top-10 left-0 z-10">
         <ShopInfo isOwner={true}/>
        </div>
+       <div className="w-[72%] rounded-[4px]">
+
+        
+       </div>
 
        </div>
       
