b7380df6ba6cbba08ebee3f89427cef6a87a2162
Aswin <aswinsudhakaran98@gmail.com>
2024-04-10 18:09:22 +0530

    update

--- src/pages/Shop/ShopHomePage.jsx
+++ src/pages/Shop/ShopHomePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import styles from '../../styles/styles'
 
 const ShopHomePage = () => {
   return (
-    <div>
-      shop
+    <div className={`${styles.section} bg-[#f5f5f5]`}>
+       <div className="w-full flex py-10 justify-between">
+
+       </div>
+      
     </div>
   )
 }
