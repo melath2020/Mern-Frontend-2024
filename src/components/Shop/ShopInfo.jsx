b805e7d7d33270269d2237051f31de549f0fca16
Aswin <aswinsudhakaran98@gmail.com>
2024-04-11 12:47:40 +0530

    update

--- src/components/Shop/ShopInfo.jsx
+++ src/components/Shop/ShopInfo.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const ShopInfo = ({ isOwner }) => {
+  const { seller } = useSelector((state) => state.seller);
+  
   return (
     <div>
       
