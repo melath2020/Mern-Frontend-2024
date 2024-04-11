de288599001d71433a336dcad3c9cb8fc97ddc39
Aswin <aswinsudhakaran98@gmail.com>
2024-04-11 12:42:50 +0530

    update

--- src/components/Shop/ShopInfo.jsx
+++ src/components/Shop/ShopInfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ShopInfo = () => {
+const ShopInfo = ({ isOwner }) => {
   return (
     <div>
       
--- src/components/Shop/ShopProfileData.jsx
+++ src/components/Shop/ShopProfileData.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ShopProfileData = () => {
+const ShopProfileData = ({ isOwner }) => {
   return (
     <div>
       sho
