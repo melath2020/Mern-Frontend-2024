92a0333a3bd566a94c36259615e3851d0135c545
Aswin <aswinsudhakaran98@gmail.com>
2024-04-11 13:02:47 +0530

    update

--- src/components/Shop/ShopInfo.jsx
+++ src/components/Shop/ShopInfo.jsx
@@ -6,7 +6,8 @@ const ShopInfo = ({ isOwner }) => {
   const { seller } = useSelector((state) => state.seller);
   
   return (
-    <div className="w-full py-5">
+  <div>
+      <div className="w-full py-5">
         <div className="w-full flex item-center justify-center">
         <img
             src={`${backend_url}${seller?.avatar}`}
@@ -20,6 +21,27 @@ const ShopInfo = ({ isOwner }) => {
         </p>
       
     </div>
+    <div className="p-3">
+        <h5 className="font-[600]">Address</h5>
+        <h4 className="text-[#000000a6]">{seller.address}</h4>
+      </div>
+      <div className="p-3">
+        <h5 className="font-[600]">Phone Number</h5>
+        <h4 className="text-[#000000a6]">{seller.phoneNumber}</h4>
+      </div>
+      {/* <div className="p-3">
+        <h5 className="font-[600]">Total Products</h5>
+        <h4 className="text-[#000000a6]">{products && products.length}</h4>
+      </div>
+      <div className="p-3">
+        <h5 className="font-[600]">Shop Ratings</h5>
+        <h4 className="text-[#000000b0]">{averageRating}/5</h4>
+      </div>
+      <div className="p-3">
+        <h5 className="font-[600]">Joined On</h5>
+        <h4 className="text-[#000000b0]">{data?.createdAt?.slice(0, 10)}</h4>
+      </div> */}
+  </div>
   )
 }
 
