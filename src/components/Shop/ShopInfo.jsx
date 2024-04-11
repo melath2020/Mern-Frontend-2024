2a03e0a82f1117025efa7e38b5225462de7d5d34
Aswin <aswinsudhakaran98@gmail.com>
2024-04-11 12:56:34 +0530

    update

--- src/components/Shop/ShopInfo.jsx
+++ src/components/Shop/ShopInfo.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { backend_url } from '../../server';
 
 const ShopInfo = ({ isOwner }) => {
   const { seller } = useSelector((state) => state.seller);
   
   return (
-    <div>
+    <div className="w-full py-5">
+        <div className="w-full flex item-center justify-center">
+        <img
+            src={`${backend_url}${seller?.avatar}`}
+            alt=""
+            className="w-[150px] h-[150px] object-cover rounded-full"
+          />
+        </div>
+        <h3 className="text-center py-2 text-[20px]">{seller.name}</h3>
+        <p className="text-[16px] text-[#000000a6] p-[10px] flex items-center">
+          {seller.description}
+        </p>
       
     </div>
   )
