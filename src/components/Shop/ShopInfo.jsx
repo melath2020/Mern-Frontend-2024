bae790a768831bef677a90baedc147a4dff5b5ab
Aswin <aswinsudhakaran98@gmail.com>
2024-04-11 14:43:11 +0530

    update

--- src/components/Shop/ShopInfo.jsx
+++ src/components/Shop/ShopInfo.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { backend_url } from '../../server';
+import styles from '../../styles/styles';
+import { Link, useParams } from "react-router-dom";
 
 const ShopInfo = ({ isOwner }) => {
   const { seller } = useSelector((state) => state.seller);
+
+  const logoutHandler = async () => {
+    
+  };
   
   return (
   <div>
@@ -41,6 +47,20 @@ const ShopInfo = ({ isOwner }) => {
         <h5 className="font-[600]">Joined On</h5>
         <h4 className="text-[#000000b0]">{seller.createdAt.slice(0,10)}</h4>
       </div>
+      {isOwner && (
+        <div className="py-3 px-4">
+           <Link to="/settings">
+           <div className={`${styles.button} !w-full !h-[42px] !rounded-[5px]`}>
+            <span className="text-white">Edit Shop</span>
+          </div>
+           </Link>
+          <div className={`${styles.button} !w-full !h-[42px] !rounded-[5px]`}
+          onClick={logoutHandler}
+          >
+            <span className="text-white">Log Out</span>
+          </div>
+        </div>
+      )}
   </div>
   )
 }
