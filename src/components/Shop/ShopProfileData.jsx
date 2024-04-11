230414d3d275f6d14a05700edd33155267f7d206
Aswin <aswinsudhakaran98@gmail.com>
2024-04-11 14:54:45 +0530

    update

--- src/components/Shop/ShopProfileData.jsx
+++ src/components/Shop/ShopProfileData.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const ShopProfileData = ({ isOwner }) => {
   return (
-    <div>
-      sho
-    </div>
-  )
-}
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+    
+          <div className="flex items-center">
+            <h5
+              className={`font-[600] text-[20px] 
+                text-red-500 cursor-pointer pr-[20px]`}
+            >
+              Shop Products
+            </h5>
+          </div>
+          <div className="flex items-center">
+            <h5
+              className={`font-[600] text-[20px]  text-red-500 
+               cursor-pointer pr-[20px]`}
+            >
+              Running Events
+            </h5>
+          </div>
 
-export default ShopProfileData
+          <div className="flex items-center">
+            <h5
+              className={`font-[600] text-[20px]  text-red-500 
+               cursor-pointer pr-[20px]`}
+            >
+              Shop Reviews
+            </h5>
+          </div>
+       
+      </div>
+    </div>
+  );
+};
+
+export default ShopProfileData;
