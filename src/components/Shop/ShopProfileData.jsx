4201b2658d55545ce2771855c52f07d3933084fe
Aswin <aswinsudhakaran98@gmail.com>
2024-04-12 10:35:28 +0530

    update

--- src/components/Shop/ShopProfileData.jsx
+++ src/components/Shop/ShopProfileData.jsx
@@ -1,31 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ShopProfileData = ({ isOwner }) => {
+  const [active, setActive] = useState(1);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center ">
     
-          <div className="flex items-center">
+          <div className="flex items-center" onClick={() => setActive(1)}>
             <h5
-              className={`font-[600] text-[20px] 
-                text-red-500 cursor-pointer pr-[20px]`}
+              className={`font-[600] text-[20px] ${
+                active === 1 ? "text-red-500" : "text-[#333]"
+              } cursor-pointer pr-[20px]`}
             >
               Shop Products
             </h5>
           </div>
-          <div className="flex items-center">
+          <div className="flex items-center" onClick={() => setActive(2)}>
             <h5
-              className={`font-[600] text-[20px]  text-red-500 
-               cursor-pointer pr-[20px]`}
+              className={`font-[600] text-[20px] ${
+                active === 2 ? "text-red-500" : "text-[#333]"
+              } cursor-pointer pr-[20px]`}
             >
               Running Events
             </h5>
           </div>
 
-          <div className="flex items-center">
+          <div className="flex items-center" onClick={() => setActive(3)}>
             <h5
-              className={`font-[600] text-[20px]  text-red-500 
-               cursor-pointer pr-[20px]`}
+              className={`font-[600] text-[20px] ${
+                active === 3 ? "text-red-500" : "text-[#333]"
+              } cursor-pointer pr-[20px]`}
             >
               Shop Reviews
             </h5>
