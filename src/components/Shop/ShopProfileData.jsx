17e14512ac55c3badffc979ca0e1e1a09da231aa
Aswin <aswinsudhakaran98@gmail.com>
2024-04-12 15:42:19 +0530

    updte

--- src/components/Shop/ShopProfileData.jsx
+++ src/components/Shop/ShopProfileData.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import { productData } from "../../static/data";
 import ProductCard from "../Route/ProductCard/ProductCard";
+import styles from "../../styles/styles";
+import { Link } from "react-router-dom";
 
 const ShopProfileData = ({ isOwner }) => {
   const [active, setActive] = useState(1);
 
   return (
     <div className="w-full">
-      <div className="flex w-full items-center ">
+      <div className="flex w-full items-center justify-between ">
     
-          <div className="flex items-center" onClick={() => setActive(1)}>
+       <div className="w-full flex">
+       <div className="flex items-center" onClick={() => setActive(1)}>
             <h5
               className={`font-[600] text-[20px] ${
                 active === 1 ? "text-red-500" : "text-[#333]"
@@ -37,6 +40,19 @@ const ShopProfileData = ({ isOwner }) => {
               Shop Reviews
             </h5>
           </div>
+       </div>
+
+       <div>
+          {isOwner && (
+            <div>
+              <Link to="/dashboard">
+                <div className={`${styles.button} !rounded-[4px] h-[42px]`}>
+                  <span className="text-[#fff]">Go Dashboard</span>
+                </div>
+              </Link>
+            </div>
+          )}
+        </div>
        
       </div>
 
