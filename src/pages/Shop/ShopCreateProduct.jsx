dd3fd879db360d842f06a4b2bf1e1a20407dd39b
Aswin <aswinsudhakaran98@gmail.com>
2024-04-16 10:46:04 +0530

    update

--- src/pages/Shop/ShopCreateProduct.jsx
+++ src/pages/Shop/ShopCreateProduct.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import DashboardHeader from '../../components/Shop/Layout/DashboardHeader'
+import DashboardSidebar from '../../components/Shop/Layout/DashboardSidebar'
+import CreateProduct from '../../components/Shop/CreateProduct'
 
 const ShopCreateProduct = () => {
   return (
     <div>
-      
+      <DashboardHeader/>
+      <div className="flex items-start justify-between w-full">
+      <div className="w-[80px] 800px:w-[330px]">
+        <DashboardSidebar active={4}/>
+
+        </div>
+        <div className="w-full justify-center flex">
+                <CreateProduct />
+            </div>
+        
+
+      </div>
     </div>
   )
 }
